0c387b3fbc056aa3ce1a957227b32974f83bda12
putchala-pranay <pputchal@student.gitam.edu>
2026-01-28 11:18:23 +0530

    Update Printing of num according to users input.py

--- Printing of num according to users input.py
+++ Printing of num according to users input.py
@@ -1,14 +1,19 @@
 first_num = int(input("Enter the first number:"))
 last_num = int(input("Enter the last number:"))
 break_num = int(input("Enter the valid number:"))
+if first_num > last_num or break_num > last_num:
+    print("Invalid input")
+    exit()
+    
 for i in range(first_num,last_num):
     if i % 2 != 0:
         continue  
     elif i == break_num:
+        print(i)
         break    
     else:
         pass    
-    
+
     print(i)
 Output:
 Enter the first number:0
