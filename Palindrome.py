e985014ba2199e87a2149891360037a99385bf2f
putchala-pranay <pputchal@student.gitam.edu>
2026-02-04 11:00:28 +0530

    Create 2 Methods to check Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,3 +1,5 @@
+Method-1:
+
 text = input("Enter a Word:")
 reversing = reversed(text)
 reversed_text="".join(reversing)
@@ -5,6 +7,14 @@ if text == reversed_text:
     print("Palindrome")
 else:
     print("Not a Palindrome")
+
+Method-2:
+text = input("Enter a Word:")
+if text = text[::-1]:  
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+
 
 OUTPUT:
 Enter a Word:RACECAR
